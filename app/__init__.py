a799822aaf11d7c25690068317294933449cca2e
ArielMarin23 <artymarin73@gmail.com>
2024-08-11 21:08:25 -0400

    Inicio del proyecto Sistema de Restaurante

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+#definir una funcion de procesador de contexto para la aplicacion
+@app.context_processor
+def inject_activate_page():
+    #Obtener la pagina activa usando request.endpoint
+    activate_page = request.endpoint
+    #devolver un diccionario con la variable active_page
+    return dict(activate_page=activate_page)
+
