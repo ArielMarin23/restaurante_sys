e166624873426f75fd58a05c033c37167631dee5
ArielMarin23 <artymarin73@gmail.com>
2024-08-23 20:11:23 -0400

    Se agrego la vista editar ciudad

--- app/rutas/referencial/ciudad/ciudad_routes.py
+++ app/rutas/referencial/ciudad/ciudad_routes.py
@@ -1,6 +1,10 @@
 from flask import Blueprint,render_template,request,redirect,url_for,jsonify
-ciumod = Blueprint('ciudad', __name__, template_folder='templates')
+ciumod = Blueprint('ciudad',__name__, template_folder='templates')
 
 @ciumod.route('/index_ciudad')
 def index_ciudad():
-    return render_template('index-ciudad.html')
+    return render_template('index_ciudad.html')
+
+@ciumod.route('/editar_ciudad')
+def editar_ciudad():
+    return render_template('editar_ciudad.html')
