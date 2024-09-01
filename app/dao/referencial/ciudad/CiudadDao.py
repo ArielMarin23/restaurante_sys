b0756f07c18db872b1ebe7b5154d4fae769c5287
ArielMarin23 <artymarin73@gmail.com>
2024-09-01 13:34:14 -0400

    Se actualiza y muestra bien la tabla ciudad

--- app/dao/referencial/ciudad/CiudadDao.py
+++ app/dao/referencial/ciudad/CiudadDao.py
@@ -1,34 +1,55 @@
 #Importacion de conexion 
 from app.conexion.Conexion import ConexionDB
+import psycopg2
 
 class CiudadDao:
-    def _ejecutarConsulta(self, consulta, params=None, fecth_one=False):
+    def _ejecutarConsulta(self, consulta, params=None, fetch_one=False, fetch_all=False):
         conexion = ConexionDB()
         con = conexion.getConexion()
         cur = con.cursor()
         try:
-            cur.execute(consulta, params)
-            if fecth_one:
+            cur.execute(consulta,params)
+            #Para consultas select
+            if fetch_one:
                 return cur.fetchone()
-            return cur.fetchall()
-        except con.Error as e:
-            print(f"pgcode ={e.pgcode}, mensaje ={e.pgerror}")
+            if fetch_all:
+                return cur.fetchall()
+            #para consultas UPDATE, INSERT, DELETE
+            con.commit()
+            return True
+        except psycopg2.Error as e:
+            con.rollback()
+            print(f"pgcode = {e.pgcod}, mensaje = {e.pgerror}")
+            return False
         finally:
             cur.close()
             con.close()
     #Se obtiene la consulta desde la BD
+    #Este metodo de  todas la ciudades en forma de tuplas
+    #ya que usa fetch_all=TRUE
     def getCiudades(self):
         ciudadSQL = """
             SELECT id, descripcion 
-            FROM ciudades      
+            FROM ciudades 
+            ORDER BY id ASC  -- Ordena  en orden ascendente     
         """
-        return self._ejecutarConsulta(ciudadSQL)
+        ciudades = self._ejecutarConsulta(ciudadSQL, fetch_all=True)  # fetch_all para obtener todas las filas
+        if ciudades is None:  # Si ocurre un error, maneja el caso
+            return []
+        return ciudades
+    #Este metodo devuelve un diccionario con los campos id y descripcion
     def getCiudadById(self,id):
         ciudadSQL = """
             SELECT id , descripcion
             FROM ciudades WHERE id = %s
         """
-        ciudad = self._ejecutarConsulta(ciudadSQL, (id),fecth_one=True)
+        ciudad = self._ejecutarConsulta(ciudadSQL, (id,),fetch_one=True)
         if ciudad:
             return {'id': ciudad[0], 'descripcion': ciudad[1]}
-        return none
+        return None
+    #Metodo de Actualizacion(Boton Editar)
+    def updateCiudad (self, id, descripcion):
+        updateSQL="""
+            UPDATE ciudades SET descripcion = %s WHERE id = %s
+        """
+        return self._ejecutarConsulta(updateSQL,(descripcion,id))
--- app/rutas/referencial/ciudad/ciudad_routes.py
+++ app/rutas/referencial/ciudad/ciudad_routes.py
@@ -1,21 +1,48 @@
 from flask import Blueprint,render_template,request,redirect,url_for,jsonify
 from app.dao.referencial.ciudad.CiudadDao import CiudadDao
+from flask import flash
+
 ciumod = Blueprint('ciudad',__name__, template_folder='templates')
+#Funcion auxiliar convierte tuplas en lista de diccionarios
+def convertir_diccionario(lista):
+    return[{'id': item[0], 'descripcion': item[1]} for item in lista]
 #Vista Principal de ciudad
+#Muestra la base datos en el html
 @ciumod.route('/index_ciudad')
 def index_ciudad():
     cdao = CiudadDao()
     lista = cdao.getCiudades()
     diccionario = convertir_diccionario(lista)
     return render_template('index_ciudad.html',ciudades=diccionario)
-#Funcion auxiliar convierte tuplas en lista de diccionarios
-def convertir_diccionario(lista):
-    return[{'id': item[0], 'descripcion': item[1]} for item in lista]
 #Vista del formulario agregar
 @ciumod.route('/agregar_ciudad')
 def agregar_ciudad():
     return render_template('agregar_ciudad.html')
 #Vista del formulario editar
-@ciumod.route('/editar_ciudad')
-def editar_ciudad():
-    return render_template('editar_ciudad.html')
+#Esta funcion trae los datos de la DB y los pasa en el html
+@ciumod.route('/editar_ciudad/<id>')
+def editar_ciudad(id):
+    cdao = CiudadDao()
+    ciudadFound = cdao.getCiudadById(id)
+    if ciudadFound:
+        return render_template('editar_ciudad.html', ciudad=ciudadFound)
+    return redirect(url_for('index_ciudad'))
+
+@ciumod.route('/update_ciudad', methods=['POST'])
+def update_ciudad():
+    cdao = CiudadDao()  # Instancia del Data Access Object (DAO)
+    
+    idtxtciudad = request.form.get('idtxtciudad')
+    txtciudad = request.form.get('txtciudad')
+    
+    # Verificación básica de los campos
+    if not idtxtciudad or not txtciudad.strip():
+        return redirect(url_for('ciudad.editar_ciudad', id=idtxtciudad))
+    
+    # Intentar actualizar la ciudad
+    isUpdate = cdao.updateCiudad(idtxtciudad.strip(), txtciudad.strip().upper())
+    
+    if isUpdate:
+        return redirect(url_for('ciudad.index_ciudad',update_status='success'))  # Redirige a la lista de ciudades después de actualizar
+    else:
+        return redirect(url_for('ciudad.editar_ciudad',update_status='error', id=idtxtciudad))  # Si falla, redirige a la edición
